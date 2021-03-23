883392c491fafb219ddac3705477f4620a106231
HelenaLC <helena@crowells.eu>
2021-03-23 17:05:53 +0100

    remove comments

--- code/06-eval_1d.R
+++ code/06-eval_1d.R
@@ -2,34 +2,14 @@ suppressPackageStartupMessages({
   library(dplyr)
   library(tidyr)
 })
-# wcs <- list(refset="CellBench", H1975=NA, type="gene", metric="avg")
-# args <- list(
-#   ref = "results/qc_ref-panc8,indrop_alpha,gene_avg.rds",
-#   sim = c("results/qc_sim-panc8,indrop_alpha,gene_avg,splatter.rds","results/qc_sim-panc8,indrop_alpha,gene_avg,SPsimSeq.rds"),
-#   res = "results/ks-panc8,indrop_alpha,gene_avg.rds"
-# )
 
-# args <- list(
-#   ref = "results/qc_ref-CellBench,H1975,gene_var.rds",
-#   sim = c("results/qc_sim-CellBench,H1975,gene_var,BASiCS.rds","results/qc_sim-CellBench,H1975,gene_var,SPsimSeq.rds"),
-#   res = "results/ks-CellBench,H1975,gene_var.rds"
-# )
-
-# 
-# args<- list(ref = "results/qc_ref-CellBench,H1975,cell_cor.rds",
-#             sim = c("results/qc_sim-CellBench,H1975,cell_cor,BASiCS.rds", "results/qc_sim-CellBench,H1975,cell_cor,SPsimSeq.rds"))
-# wcs <- list(refset="CellBench", H1975=NA, type="gene", metric="avg")
-this_metric <- paste0(wcs$type, '_', wcs$metric)
-this_metric
-if(this_metric != c("cell_cms")){
 df <- .read_res(args$ref, args$sim)
-print(head(df))
-}else{df <- NA}
+type_metric <- paste(wcs$type, wcs$metric, sep = "_")
 
-source(args$fun)
-
-#TODO: take out this_metric != "cell_cms" once cms was implemented with proper parameters and does not only have zero values
-if (!is.na(df)) {
+if (isTRUE(is.na(df))) {
+  res <- NA
+} else {
+  source(args$fun)
   df <- group_by(df, group, id)
   res <- group_map(df, ~{
     df <- group_by(.x, method)
@@ -38,8 +18,8 @@ if (!is.na(df)) {
       group_keys(df)[[1]])
     i <- which(names(dfs) == "ref")
     vapply(dfs[-i], function(sim) 
-      fun(sim[[this_metric]], 
-          dfs[[i]][[this_metric]]),
+      fun(sim[[type_metric]], 
+          dfs[[i]][[type_metric]]),
       numeric(1))
   })
   res <- data.frame(
@@ -49,12 +29,8 @@ if (!is.na(df)) {
       cols = -c(group, id), 
       names_to = "method", 
       values_to = "stat")
-  res <- cbind(data.frame(refset = wcs$refset, 
-                          subset = names(wcs)[2], 
-                          type= wcs$type, 
-                          metric=wcs$metric), res)
-
-} else res <- NA
+  res <- data.frame(wcs, res)
+}
 
 print(head(res))
 saveRDS(res, args$res)
