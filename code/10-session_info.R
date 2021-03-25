18f7eeb65e7a75642587dfb47051f2c271fc2960
HelenaLC <helena@crowells.eu>
2021-03-25 13:31:54 +0100

    update si

--- code/10-session_info.R
+++ code/10-session_info.R
@@ -1,3 +1,40 @@
-library(ggplot2)
+suppressPackageStartupMessages({
+    library(emdist)
+    library(ExperimentHub)
+    library(dplyr)
+    library(jsonlite)
+    library(ggplot2)
+    library(matrixStats)
+    library(patchwork)
+    library(Peacock.test)
+    library(POWSC)
+    library(purrr)
+    library(scater)
+    library(scran)
+    library(SeuratData)
+    library(splatter)
+    library(SPsimSeq)
+    library(SymSim)
+    library(tidyr)
+})
 
 writeLines(capture.output(sessionInfo()), args[[1]])
+
+# pkgs <- c(
+#     "BASiCS",
+#     "CellBench",
+#     "emdist", "ExperimentHub",
+#     "dplyr",
+#     "jsonlite",
+#     "ggplot2",
+#     "matrixStats",
+#     "patchwork", "Peacock.test", "suke18/POWSC", "purrr", "bvieth/powsimR",
+#     "Vivianstats/scDesign", "JSB-UCLA/scDesign2",
+#     "scater", "scran", "Seurat", "satijalab/seurat-data", "splatter", "SPsimSeq", "YosefLab/SymSim",
+#     "tidyr", "variancePartition")
+# 
+# install.packages("BiocManager")
+# BiocManager::install(pkgs, ask = FALSE, dependencies = TRUE)
+# 
+# install.packages("devtools")
+# devtools::install_gitlab("sysbiobig/sparsim")
