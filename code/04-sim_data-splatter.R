7327d3feae289f3e4b8d80bb4c2d53fb70eb5479
HelenaLC <helena@crowells.eu>
2021-03-31 09:25:43 +0200

    revert Splatter to only type n

--- code/04-sim_data-splatter.R
+++ code/04-sim_data-splatter.R
@@ -4,20 +4,9 @@ suppressPackageStartupMessages({
 })
 
 fun <- function(x) {
-    nb <- getParam(x$params, "nBatches")
-    nk <- getParam(x$params, "nGroups")
-    method <- ifelse(nb > 1 || nk > 1, "groups", "single")
-    y <- splatSimulate(x$params, method, verbose = FALSE)
-    # keep variables of interest only
-    nc <- getParam(x$params, "nCells")
-    cd <- make_zero_col_DFrame(nc)
-    if (nb > 1) cd$batch <- factor(y$Batch, labels = x$ids$batch)
-    if (nk > 1) cd$cluster <- factor(y$Group, labels = x$ids$cluster)
-    colData(y) <- cd
-    # drop gene and global metadata
-    rowData(y) <- NULL
+    y <- splatSimulate(x, verbose = FALSE)
     metadata(y) <- list()
-    # keep counts only
+    rowData(y) <- colData(y) <- NULL
     assays(y) <- assays(y)["counts"]
     return(y)
 }
