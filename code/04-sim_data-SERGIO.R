b34568a28c68afc6d9085c93f9467b076b2dea43
HelenaLC <helena.crowell@uzh.ch>
2021-04-06 08:02:14 +0200

    add SERGIO

--- code/04-sim_data-SERGIO.R
+++ code/04-sim_data-SERGIO.R
@@ -1,3 +1,15 @@
+# need to set path in .Rprofile
+# Sys.setenv(RETICULATE_PYTHON = "/Users/sarahmorillo/anaconda3/envs/sim_comp/bin/python")
+
+suppressPackageStartupMessages({
+  library(reticulate)
+  library(SingleCellExperiment)
+})
+
+use_condaenv(condaenv = "sim_comp", required = TRUE)
+
 fun <- function(x){
-  return(x)
+    source_python("code/03-est_pars-SERGIO.py")
+    y <- as.matrix(simulate())
+    SingleCellExperiment(list(counts = y))
 }
